a390588c04f1dbbfb378a103043ad947e50a8256
Marco Arruda <maanton2@student.42sp.org.br>
2024-11-07 23:24:33 -0300

    FEAT: Base header

---
+++ get_next_line.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maanton2 <maanton2@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/31 17:10:37 by maanton2          #+#    #+#             */
+/*   Updated: 2024/11/07 05:53:22 by maanton2         ###   ########.org.br   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <unistd.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include <stdlib.h>
+
+char		*get_next_line(int fd);
+size_t		ft_strlen(const char *s);
+char		*ft_strdup(const char *s);
+char		*ft_strchr(const char *s, int c);
+char		*ft_strjoin(char const *s1, char const *s2);
+void		*ft_memcpy(void *dest, const void *src, size_t n);
+
+#endif
